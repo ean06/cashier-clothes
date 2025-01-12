cbdac5109d56bfa3046095a1543f66e978f04637
ean06 <raihanfadilah0406@gmail.com>
2025-01-12 18:21:57 +0700

    DiscountModel

---
+++ app/Models/DiscountModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DiscountModel extends Model
+{
+    use HasFactory;
+}
