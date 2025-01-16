cf8601cea797d485564eafbb65228e192ba6a023
ean06 <raihanfadilah0406@gmail.com>
2025-01-16 23:13:34 +0700

    Voucher Controller

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class VoucherController extends Controller
 {
-    //
+    public function editVoucher(Request $request): View
+    {
+        return view('Voucher.edit', [
+            'user' => $request->user(),
+        ]);
+    }
 }
