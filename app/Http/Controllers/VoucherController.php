45fe21b662317ae6b7c52ea9dcc06b16afa4af97
ean06 <raihanfadilah0406@gmail.com>
2025-01-17 17:26:26 +0700

    Update Voucher

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -6,10 +6,26 @@ use Illuminate\Http\Request;
 
 class VoucherController extends Controller
 {
+
+    // VIEW
     public function editVoucher(Request $request): View
     {
         return view('Voucher.edit', [
             'user' => $request->user(),
         ]);
+    }
+
+    // UPDATE
+    public function update(upadteVoucher $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            $request->user()->email_verified_at = null;
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 }
