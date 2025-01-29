b590ccee2de90a44699528b207b8c55d4a319455
ean06 <raihanfadilah0406@gmail.com>
2025-01-29 21:50:52 +0700

    ProdukModel

--- app/Models/ProdukModel.php
+++ app/Models/ProdukModel.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ProdukModel extends Model
 {
     use HasFactory;
+
+    // Menentukan nama tabel jika berbeda dengan nama model
+    protected $table = 'produk';
+
+    // Menentukan kolom yang dapat diisi (mass assignable)
+    protected $fillable = [
+        'nama', 'deskripsi', 'harga', 'stok'
+    ];
+
+    // Menentukan kolom yang tidak bisa diisi (guarded)
+    // protected $guarded = ['id'];
+
+    // Menentukan tipe atribut untuk konversi otomatis
+    protected $casts = [
+        'harga' => 'decimal:2', // harga akan otomatis diformat menjadi tipe decimal
+    ];
 }
