eb9b3165bfac03f0758af7a0afa2c453962735df
ean06 <raihanfadilah0406@gmail.com>
2025-01-31 05:53:17 +0700

    Voucher Model

--- app/Models/VoucherModel.php
+++ app/Models/VoucherModel.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class VoucherModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code', 'discount', 'valid_from', 'valid_until'
+    ];
+
+    // Menambahkan scope untuk validasi voucher yang masih berlaku
+    public function scopeValid($query)
+    {
+        return $query->where('valid_from', '<=', now())
+                    ->where('valid_until', '>=', now());
+    }
 }
