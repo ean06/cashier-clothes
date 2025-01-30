12327b31270c8ac13db75009062dfb10dc929fb2
ean06 <raihanfadilah0406@gmail.com>
2025-01-30 16:45:58 +0700

    transaksi model

--- app/Models/TransaksiModel.php
+++ app/Models/TransaksiModel.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class TransaksiModel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'transaction_code', 'amount', 'status', 'user_id',
+    ];
+
+    // Relasi dengan User (jika ada)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
